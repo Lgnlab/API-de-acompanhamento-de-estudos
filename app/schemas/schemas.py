e87cc50aa869c8594b53817f23004435605c941c
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-07-10 12:56:42 -0300

    Definição dos schemas

---
+++ app/schemas/schemas.py
@@ -0,0 +1,43 @@
+from datetime import date
+from pydantic import BaseModel, EmailStr, Field
+
+class UsuarioCreate(BaseModel):
+    nome: str
+    email: EmailStr
+    senha: str
+
+class UsuarioResponse(BaseModel):
+    id: int
+    nome: str
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+
+class MateriaCreate(BaseModel):
+    materia: str
+
+class MateriaResponse(BaseModel):
+    id: int
+    materia: str
+
+
+    class Config:
+        from_attributes = True
+    
+
+class SessaoEstudoCreate(BaseModel):
+    data: date
+    horas: float = Field(gt=0)
+    concluida: bool = False
+
+
+class SessaoEstudoResponse(BaseModel):
+    id: int
+    data: date
+    horas: float
+    concluida: bool
+
+    class Config:
+        from_attributes = True
