0afba5ebeac34f3d68b8bc9613978897e68313a1
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-07-13 12:10:58 -0300

    Proteção das rotas

--- app/core/security.py
+++ app/core/security.py
@@ -3,12 +3,18 @@ from datetime import datetime, timedelta, timezone
 import jwt
 from dotenv import load_dotenv
 import os
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+from sqlalchemy.orm import Session
+from app.database.database import get_db
+from app.models.models import Usuario
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES= os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES= int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -30,3 +36,28 @@ def criar_token(dados: dict) -> str:
     )
 
     token = jwt.encode(dados_token, SECRET_KEY, algorithm=ALGORITHM)
+
+    return token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+def verificar_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except (ExpiredSignatureError, InvalidTokenError):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Não foi possível validar as credenciais.")
+
+def get_usuario_atual(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = verificar_token(token)
+    email = payload.get("sub")
+
+    if email is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido.")
+    
+    usuario = (db.query(Usuario).filter(Usuario.email == email).first())
+    if usuario is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Usuário não encontrado.")
+    
+    return usuario
